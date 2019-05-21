053f0db3e2009738b0bc84fd59df9e50903c928b
cksgh94a <cksgh94a@naver.com>
2019-05-21 20:12:20 +0900

    1

--- EclipseTest/src/Test.java
+++ EclipseTest/src/Test.java
@@ -1,27 +1,64 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Test {
-	static int[] n;
-	static int t, r;
 	
-	static int count;
-	
-	static void sum(int sum, int index) {
-		if (index == n.length && sum == t) r++;
-		else if (index < n.length) {
-			sum(sum + n[index], index+1);
-			sum(sum - n[index], index+1);			
-		}	
+	public static int road(int[][] map, int k, int posX, int posY, int prevX, int prevY) {
+		int length = 0;
+
+		if (map[posX][posY] == 1 ) return 1;	// ���� �� ����
+		else {
+			if (k > 0) {	// ���� ���� ����
+			} else {	// ���� ���� �Ұ�
+			}
+
+			return length+1;	
+		}		
 	}
 	
 	public static void main(String[] args) {
-		int[] numbers = {1,1,1,1,1};
-		int target = 3;
+		Scanner sc = new Scanner(System.in);
+				
+		int t = Integer.parseInt(sc.nextLine());
+		int n,k;
 		
-		n = numbers;
-		t = target;
-
-		sum(0, 0);
+		String nk[];
+		String temp[];
+		int map[][];		
+		int maxH = 0 ;	// ���츮 ����
+		int maxLength = 0;	// ���� ����
+		int length = 0;
 		
-		System.out.println(r);
+		for (int i = 0 ; i < t; i ++) {
+			nk = sc.nextLine().split(" ");
+			n = Integer.parseInt(nk[0]);
+			k = Integer.parseInt(nk[1]);
+			
+			map = new int[n][n];
+			
+			// �� ����
+			for (int y = 0; y < n; y++) {
+				temp = sc.nextLine().split(" ");
+				for (int x = 0; x < n; x++) {
+					map[x][y] = Integer.parseInt(temp[y]);
+					if (map[x][y] > maxH) maxH = map[x][y];
+				}
+			}
+			
+			// ���� Ž��
+			for (int y = 0; y < n; y++) {
+				for (int x = 0; x < n; x++) {
+					if (map[x][y] == maxH) length = road(map, k, x, y, x, y);
+					if (length > maxLength) maxLength = length;
+				}
+			}
+			
+			System.out.println("#" + (i+1) + " " + (maxLength-1));
+		}
+		
+		sc.close();
 	}
-
 }
+
+/*
+*/
